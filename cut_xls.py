17b5a44a104760cd824875bc0efa1faaa26dcf39
dekarh <dekarh3@gmail.com>
2017-08-23 06:38:43 +0400

    Сделал и отладил

---
+++ cut_xls.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+import openpyxl
+import sys
+import time
+import csv
+from datetime import datetime
+from lib import l, lenl
+
+IN_SNILS = ['СНИЛС', 'СтраховойНомер', 'Страховой_номер', 'Страховой Номер', 'Номер СНИЛС']
+
+
+fields = []
+workbooks =  []
+sheets = []
+for i, xlsx_file in enumerate(sys.argv):                              # Загружаем все xlsx файлы
+    if i == 0:
+        continue
+    workbooks.append(openpyxl.load_workbook(filename=xlsx_file, read_only=True))
+    sheets.append(workbooks[i-1][workbooks[i-1].sheetnames[0]])
+
+sheets_keys = []
+total_rows = 0
+for i, sheet in enumerate(sheets):                                    # Маркируем нужные столбцы
+    total_rows += sheet.max_row
+    keys = {}
+    for j, row in enumerate(sheet.rows):
+        if j > 0:
+            break
+        for k, cell in enumerate(row):                                # Проверяем, чтобы был СНИЛС и Код
+            if cell.value in IN_SNILS:
+                keys[IN_SNILS[0]] = k
+        if len(keys) < 1:
+            print('В файле "' + sys.argv[i+1] + '" отсутствует колонка со СНИЛС')
+            time.sleep(3)
+            sys.exit()
+    sheets_keys.append(keys)
+
+a = sys.argv[1]
+if len(a.split('/')) > 1:
+    path = '/'.join(a.split('/')[:len(a.split('/'))-1])+'/'              # только путь без имени файла
+else:
+    path = ''
+
+
+print('\n'+ datetime.now().strftime("%H:%M:%S") +' Начинаем преобразование и нарезку xlsx файлов \n')
+
+k = 1                                                           # Счетчик строк в csv
+file_number = 1
+cl_csvs = []
+for i, sheet in enumerate(sheets):                              # Загружаем все xlsx файлы по мере сохранения в БД
+    for j, row in enumerate(sheet.rows):                              # Теперь строки
+        if j == 0:
+            continue
+        cl_csv = {}
+        if lenl(row[keys[IN_SNILS[0]]].value) < 12: # and l(row[keys[IN_SNILS[0]]].value) > 100:
+            cl_csv[IN_SNILS[0]] = '{:=011d}'.format(l(row[keys[IN_SNILS[0]]].value))
+            cl_csvs.append(cl_csv)
+        else:
+            print('\nСНИЛС ' + str(row[keys[IN_SNILS[0]]].value) + ' пропущен\n' )
+        if k % 15000 == 0:
+            with open(path + '{:=03d}'.format(file_number) + '.csv', 'w', encoding='cp1251') as output_file:
+                dict_writer = csv.DictWriter(output_file, [IN_SNILS[0]], delimiter=';')  # , quoting=csv.QUOTE_NONNUMERIC)
+                dict_writer.writeheader()
+                dict_writer.writerows(cl_csvs)
+            output_file.close()
+            cl_csvs = []
+            k = 0
+            print(datetime.now().strftime("%H:%M:%S") + ' 15k из файла '+ sys.argv[i+1].split(path)[1] +' сохранил в файл '
+                  + '{:=03d}'.format(file_number) + '.csv')
+            file_number += 1
+        k += 1
+
+    print('\n' + datetime.now().strftime("%H:%M:%S") + ' Файл '+ sys.argv[i+1].split(path)[1] +' загружен полностью\n')
+with open(path + '{:=03d}'.format(file_number) + '.csv', 'w', encoding='cp1251') as output_file:
+    dict_writer = csv.DictWriter(output_file, [IN_SNILS[0]], delimiter=';')  # , quoting=csv.QUOTE_NONNUMERIC)
+    dict_writer.writeheader()
+    dict_writer.writerows(cl_csvs)
+output_file.close()
+print(datetime.now().strftime("%H:%M:%S") + ' Остаток из файла '+ sys.argv[i+1].split(path)[1] +' сохранил в файл '
+                  + '{:=03d}'.format(file_number) + '.csv')
+
+print('\n'+ datetime.now().strftime("%H:%M:%S") +' Завершено без сбоев\n')
---
+++ lib.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# Общая библиотека функций
+# ver 1.01
+
+import string
+from configparser import ConfigParser
+
+def lenl(a):            # длинна белиберды переведнной в цифры или 0
+    try:
+        if a != None:
+            a = str(a).strip()
+            if  a != '':
+                a = ''.join([char for char in a if char in string.digits])
+                return len(a)
+        return 0
+    except TypeError:
+        return 0
+
+def l(a):               # белиберду в цифры или 0
+    try:
+        if a != None:
+            a = str(a).strip()
+            if  a != '':
+                a = ''.join([char for char in a if char in string.digits])
+                if len(a) > 0:
+                    return int(a)
+                else:
+                    return 0
+        return 0
+    except TypeError:
+        return 0
+
+def s(a):                   # белиберду в строку
+    try:
+        if a != None:
+            return str(a).strip().replace(u"\xa0", u" ")
+        return ''
+    except TypeError:
+        return ''
+
+def unique(lst):            # сделать список уникальным
+    seen = set()
+    j = 0
+    while j < len(lst)-1:
+        for i, x in enumerate(lst):
+            j = i
+            if x.lower() in seen:
+                lst.pop(i)
+                seen = set()
+                break
+            seen.add(x.lower())
+    return
+
+def format_police_code(code):# форматирование любого числа в код подразделения 2 => '000-002'
+    if lenl(code) < 7:
+        return '{:=06d}'.format(l(code))[:3]+'-'+'{:=06d}'.format(l(code))[3:]
+    else:
+        return '111-111'
+
+def read_config(filename='config.ini', section='mysql'):
+    """ Read database configuration file and return a dictionary object
+    :param filename: name of the configuration file
+    :param section: section of database configuration
+    :return: a dictionary of database parameters
+    """
+    # create parser and read ini configuration file
+    parser = ConfigParser()
+    parser.read(filename)
+
+    # get section, default to mysql
+    db = {}
+    if parser.has_section(section):
+        items = parser.items(section)
+        for item in items:
+            db[item[0]] = item[1]
+    else:
+        raise Exception('{0} not found in the {1} file'.format(section, filename))
+
+    return db
